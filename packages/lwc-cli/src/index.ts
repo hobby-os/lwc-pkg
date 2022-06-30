81c490734d8497c6c1f4837077584ac75cdcc219
liuwenchen <liuwenchen>
2022-06-30 14:38:10 +0800

    initialize some code

---
+++ packages/lwc-cli/src/index.ts
@@ -0,0 +1,5 @@
+export type BundleOption = {
+    minify?: boolean;
+    external?: string[];
+  };
+  
---
+++ packages/lwc-ui/button/src/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    ref="_ref"
+    
+  >
+    ddd
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
